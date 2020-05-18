4ea1f42045f7855cf9ffafbcfcad4597e2178eb5
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-05-18 14:37:49 +0200

    Mise à jour de acceuil (BDD)

--- Code/acceuil.php
+++ Code/acceuil.php
@@ -10,56 +10,71 @@
         <link rel="stylesheet" type="text/css" href="stylesheet.css">
     </head>
         <body>
-        
+          <div id="main" class="center_tile">
+
             <?php
             
-                //var_dump($_GET);
+                $bdd = new PDO('mysql:host=localhost;dbname=CYN;charset=utf8', 'root', '');
 
-            ?>
-<div class="center_tile">
-        <div style="display: inline-block;" class="tile"> 
-            Nom <br>
-            Adresse <br>
-            Date <br>
-            Heure <br>
-            Theme <br>
-            Prix <br>
-            Places <br>
-            Ect.... <br>
-        </div>
-        <div style="display: inline-block;" class="tile"> 
-            Nom <br>
-            Adresse <br>
-            Date <br>
-            Heure <br>
-            Theme <br>
-            Prix <br>
-            Places <br>
-            Ect.... <br>
-        </div>
+                $req = $bdd->query('SELECT COUNT(ID) as total from soirees');
+                $donnees = $req->fetch();
+                $nb_soirees = $donnees['total'];
 
-        <br>
+                $req = $bdd->query('SELECT * from soirees');
+                $donnees = $req->fetchAll();
+                $soirees = $donnees;
 
-        <div style="display: inline-block;" class="tile"> 
-            Nom <br>
-            Adresse <br>
-            Date <br>
-            Heure <br>
-            Theme <br>
-            Prix <br>
-            Places <br>
-            Ect.... <br>
-        </div>
-        <div style="display: inline-block;" class="tile"> 
-            Nom <br>
-            Adresse <br>
-            Date <br>
-            Heure <br>
-            Theme <br>
-            Prix <br>
-            Places <br>
-            Ect.... <br>
-        </div>
-</div>
+                for($count = 0;$count != $nb_soirees;$count++){
+                    $count2 = 1;
+                    $soiree_ID = $count + 1;
+
+                    $req = $bdd->query('SELECT Nom FROM ecoles WHERE ID IN (SELECT ecoles_ID FROM ecoles_has_associations WHERE ID IN (SELECT ecoles_has_associations_ID FROM organisateurs WHERE soirees_ID = "'.$soiree_ID.'"))');
+                    $donnees = $req->fetchAll();
+                    $orga = $donnees;
+
+                    $req = $bdd->query('SELECT COUNT(ID) FROM ecoles WHERE ID IN (SELECT ecoles_ID FROM ecoles_has_associations WHERE ID IN (SELECT ecoles_has_associations_ID FROM organisateurs WHERE soirees_ID = "'.$soiree_ID.'"))');
+                    $donnees = $req->fetch();
+                    $orga_count = $donnees[0]; 
+           ?>
+
+
+                <button class="openbtn" onclick="openNav()">&#9776;</button>
+
+                <div style="display: inline-block;" class="tile"> 
+                    <h2><a href="display_tile.php"><?php echo $soirees[$count][1];?></a></h2><br>
+                    Lizard Lounge<br>
+                    18 Mai 2020<br>
+                    23h - 06h<br>
+                    Prix : <?php echo $soirees[$count]["Prix"];?>€
+                    Nombre de places restantes : <?php echo $soirees[$count]["Places"];?><br>
+                    Organisée par : <?php for($i = 0;$i != $orga_count;$i++){echo $orga[$i][0]; if($i != $orga_count-1){echo ", ";}}?>
+                </div>
+                <?php }?>
+
+            </div>
+                
+
+            <div id="mySidebar" class="sidebar">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Nos écoles partenaires</a>
+                <a href="#">Nos associations partenaires</a>
+                <a href="#">Nous contacter</a>
+                <a href="#">A propos</a>
+            </div>
+            
+
+            <script>
+
+                function openNav() {
+                    document.getElementById("mySidebar").style.width = "250px";
+                    document.getElementById("main").style.marginLeft = "250px";
+                }
+
+
+                function closeNav() {
+                    document.getElementById("mySidebar").style.width = "0";
+                    document.getElementById("main").style.marginLeft = "0";
+                }
+            </script>
         </body>
 </html>
