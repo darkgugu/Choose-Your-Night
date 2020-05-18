3b2d1d063ddc0b38864adeeb7e716f4b2ec5ee70
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-05-18 07:50:54 +0200

    Fin de insert.sql (à tester)

--- BDD/insert.sql
+++ BDD/insert.sql
@@ -17,3 +17,36 @@ INSERT INTO associations (Nom, Adresse, Note) VALUES ('LABEL', 'XX Rue des Docte
 INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDS Ecole TEST', 'xxxxxxxxxxxxxxxx', '5');
 INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDA Fac TEST', 'xxxxxxxxxxxxxxxx', '5');
 INSERT INTO associations (Nom, Adresse, Note) VALUES ('Radio Institut TEST', 'xxxxxxxxxxxxxx', '5');
+
+DELETE FROM ecoles_has_associations;
+ALTER TABLE ecoles_has_associations AUTO_INCREMENT =0;
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('1', '1');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('2', '2');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('1', '3');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('2', '4');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('3', '5');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('4', '6');
+INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('5', '7');
+
+DELETE FROM soirees;
+ALTER TABLE soirees AUTO_INCREMENT =0;
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°1', '1 Rue Mozart','2020-06-01 23:00:00', 'Alice In Wonderland', '15', '../Images/Affiches/affiche_soiree1.jpg', '100', 'Billeterie.link.fr', 'Bar', 'DJ 1', 'dj.link.fr', 'A venir', '1');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°2', '2 Rue Mozart','2020-06-02 23:00:00', 'Alice In Wonderland', '25', '../Images/Affiches/affiche_soiree2.jpg', '200', 'Billeterie.link.fr', 'Bar', 'DJ 2', 'dj.link.fr', 'A venir', '2');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°3', '3 Rue Mozart','2020-06-03 23:00:00', 'Alice In Wonderland', '35', '../Images/Affiches/affiche_soiree3.jpg', '300', 'Billeterie.link.fr', 'Salle', 'DJ 3', 'dj.link.fr', 'A venir', '3');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°4', '4 Rue Mozart','2020-06-04 23:00:00', 'Alice In Wonderland', '45', '../Images/Affiches/affiche_soiree4.jpg', '400', 'Billeterie.link.fr', 'Salle', 'DJ 4', 'dj.link.fr', 'A venir', '4');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°5', '5 Rue Mozart','2020-06-05 23:00:00', 'Alice In Wonderland', '55', '../Images/Affiches/affiche_soiree5.jpg', '500', 'Billeterie.link.fr', 'Salle', 'DJ 5', 'dj.link.fr', 'A venir', '5');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°6', '6 Rue Mozart','2020-06-06 23:00:00', 'Alice In Wonderland', '65', '../Images/Affiches/affiche_soiree6.jpg', '600', 'Billeterie.link.fr', 'Salle', 'DJ 6', 'dj.link.fr', 'A venir', '6');
+
+DELETE FROM Organisateurs;
+ALTER TABLE Organisateurs AUTO_INCREMENT =0;
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('1', '1');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('1', '2');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('2', '3');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('2', '4');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('3', '5');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('3', '6');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('3', '7');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('4', '5');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('5', '6');
+INSERT INTO Organisateurs (soirees_ID, ecoles_has_associations_ID) VALUES ('6', '7');
+
