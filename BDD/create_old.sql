e63be4484003474b5e8bc6ed21d843585e26478c
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-05-28 04:20:58 +0200

    Changements dans la base de données

--- BDD/create_old.sql
+++ BDD/create_old.sql
@@ -1,10 +1,11 @@
--- Mon May 25 12:57:11 2020
+-- Mon May 18 08:33:29 2020
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-DROP DATABASE IF EXISTS `CYN`;
-CREATE DATABASE IF NOT EXISTS `CYN`;
+DROP DATABASE IF EXISTS `CYN` ;
+
+CREATE SCHEMA IF NOT EXISTS `CYN` DEFAULT CHARACTER SET utf8 ;
 USE `CYN`;
 
 -- -----------------------------------------------------
@@ -18,9 +19,6 @@ CREATE TABLE IF NOT EXISTS `CYN`.`ecoles` (
   `Adresse` VARCHAR(45) NULL,
   `Numero` VARCHAR(45) NULL,
   `Responsable` VARCHAR(45) NULL,
-  `Etudes` VARCHAR(45) NULL,
-  `Ratio` INT NULL,
-  `Lien` VARCHAR(45) NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
@@ -36,10 +34,6 @@ CREATE TABLE IF NOT EXISTS `CYN`.`utilisateurs` (
   `email` VARCHAR(255) NULL,
   `password` VARCHAR(32) NOT NULL,
   `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `raison` VARCHAR(45) NULL,
-  `nom` VARCHAR(45) NULL,
-  `numero` VARCHAR(45) NULL,
-  `pref` INT NULL,
   PRIMARY KEY (`ID`));
 
 
@@ -53,7 +47,6 @@ CREATE TABLE IF NOT EXISTS `CYN`.`associations` (
   `Nom` VARCHAR(45) NULL,
   `Adresse` VARCHAR(45) NULL,
   `Note` INT NULL,
-  `Lien` VARCHAR(45) NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
@@ -67,20 +60,17 @@ CREATE TABLE IF NOT EXISTS `CYN`.`soirees` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Nom` VARCHAR(45) NULL,
   `Adresse` VARCHAR(45) NULL,
-  `Lieu_nom` VARCHAR(45) NULL,
   `Date` DATETIME NULL,
-  `Heure_début` VARCHAR(45) NULL,
-  `Heure_fin` VARCHAR(45) NULL,
   `Theme` VARCHAR(45) NULL,
   `Prix` INT NULL,
   `Affiche` VARCHAR(45) NULL,
   `Places` INT NULL,
-  `Places_restantes` INT NULL,
   `Billeterie` VARCHAR(45) NULL,
   `Lieu_type` ENUM("Bar", "Salle") NULL,
   `DJ` VARCHAR(45) NULL,
   `DJ_lien` VARCHAR(45) NULL,
   `Etat` VARCHAR(45) NULL,
+  `Durée` INT NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
@@ -106,6 +96,7 @@ CREATE TABLE IF NOT EXISTS `CYN`.`ecoles_has_associations` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `CYN`.`Organisateurs`
--- BDD/insert.sql
+++ BDD/insert.sql
@@ -2,21 +2,21 @@ USE `mydb`;
 
 DELETE FROM ecoles;
 ALTER TABLE ecoles AUTO_INCREMENT =0;
-INSERT INTO ecoles (Nom, Adresse, Numero, Responsable) VALUES ('ESIEA Paris', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '0111111111', 'Sophie Galinat');
-INSERT INTO ecoles (Nom, Adresse, Numero, Responsable) VALUES ('ESIEA Laval', 'XX Rue des Docteurs Calmette et Guérin, 53XXX Laval', '0222222222', 'Sophie Galinat');
-INSERT INTO ecoles (Nom, Adresse, Numero, Responsable) VALUES ('Ecole TEST', '86 Rue Olivier de Serres, 75015 Paris', '0333333333', 'Jean Durand');
-INSERT INTO ecoles (Nom, Adresse, Numero, Responsable) VALUES ('Fac TEST', '28 Rue Rosenwald, 75015 Paris', '0444444444', 'Joris Dupond');
-INSERT INTO ecoles (Nom, Adresse, Numero, Responsable) VALUES ('Institut TEST', '4 Rue Eugene Gibez, 75015', '0555555555', 'Jahel Dinosaure');
+INSERT INTO ecoles (Nom, Adresse, Numero, Responsable, Etudes, Ratio, Lien) VALUES ('ESIEA Paris', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '0111111111', 'Sophie Galinat', 'Ingénieur', '15', 'esiea.fr');
+INSERT INTO ecoles (Nom, Adresse, Numero, Responsable, Etudes, Ratio, Lien) VALUES ('ESIEA Laval', 'XX Rue des Docteurs Calmette et Guérin, 53XXX Laval', '0222222222', 'Sophie Galinat', 'Ingénieur', '15', 'esiea.fr');
+INSERT INTO ecoles (Nom, Adresse, Numero, Responsable, Etudes, Ratio, Lien) VALUES ('Ecole TEST', '86 Rue Olivier de Serres, 75015 Paris', '0333333333', 'Jean Durand', 'Infirmière', '85', 'infirmiere.fr');
+INSERT INTO ecoles (Nom, Adresse, Numero, Responsable, Etudes, Ratio, Lien) VALUES ('Fac TEST', '28 Rue Rosenwald, 75015 Paris', '0444444444', 'Joris Dupond', 'Art Plastiques', '75', 'art.fr');
+INSERT INTO ecoles (Nom, Adresse, Numero, Responsable, Etudes, Ratio, Lien) VALUES ('Institut TEST', '4 Rue Eugene Gibez, 75015', '0555555555', 'Jahel Dinosaure', 'Informatique', '5', 'informatique.fr');
 
 DELETE FROM associations;
 ALTER TABLE associations AUTO_INCREMENT =0;
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDE Madness', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDE Osiris', 'XX Rue des Docteurs Calmette et Guérin', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('Association Internationale ESIEA', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('LABEL', 'XX Rue des Docteurs Calmette et Guérin', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDS Ecole TEST', 'xxxxxxxxxxxxxxxx', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('BDA Fac TEST', 'xxxxxxxxxxxxxxxx', '5');
-INSERT INTO associations (Nom, Adresse, Note) VALUES ('Radio Institut TEST', 'xxxxxxxxxxxxxx', '5');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('BDE Madness', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '5', 'bdem.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('BDE Osiris', 'XX Rue des Docteurs Calmette et Guérin', '5', 'bdeo.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('Association Internationale ESIEA', '74B Avenue Maurice Thorez, 94200 Ivry-sur-Seine', '5', 'international.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('LABEL', 'XX Rue des Docteurs Calmette et Guérin', '5', 'label.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('BDS Ecole TEST', 'xxxxxxxxxxxxxxxx', '5', 'bds.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('BDA Fac TEST', 'xxxxxxxxxxxxxxxx', '5', 'bda.fr');
+INSERT INTO associations (Nom, Adresse, Note, Lien) VALUES ('Radio Institut TEST', 'xxxxxxxxxxxxxx', '5', 'radio.fr');
 
 DELETE FROM ecoles_has_associations;
 ALTER TABLE ecoles_has_associations AUTO_INCREMENT =0;
@@ -30,12 +30,12 @@ INSERT INTO ecoles_has_associations (ecoles_ID, associations_ID) VALUES ('5', '7
 
 DELETE FROM soirees;
 ALTER TABLE soirees AUTO_INCREMENT =0;
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°1', '1 Rue Mozart','2020-06-01 23:00:00', 'Alice In Wonderland', '15', '../Images/Affiches/affiche_soiree1.jpg', '100', 'Billeterie.link.fr', 'Bar', 'DJ 1', 'dj.link.fr', 'A venir', '1');
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°2', '2 Rue Mozart','2020-06-02 23:00:00', 'Alice In Wonderland', '25', '../Images/Affiches/affiche_soiree2.jpg', '200', 'Billeterie.link.fr', 'Bar', 'DJ 2', 'dj.link.fr', 'A venir', '2');
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°3', '3 Rue Mozart','2020-06-03 23:00:00', 'Alice In Wonderland', '35', '../Images/Affiches/affiche_soiree3.jpg', '300', 'Billeterie.link.fr', 'Salle', 'DJ 3', 'dj.link.fr', 'A venir', '3');
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°4', '4 Rue Mozart','2020-06-04 23:00:00', 'Alice In Wonderland', '45', '../Images/Affiches/affiche_soiree4.jpg', '400', 'Billeterie.link.fr', 'Salle', 'DJ 4', 'dj.link.fr', 'A venir', '4');
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°5', '5 Rue Mozart','2020-06-05 23:00:00', 'Alice In Wonderland', '55', '../Images/Affiches/affiche_soiree5.jpg', '500', 'Billeterie.link.fr', 'Salle', 'DJ 5', 'dj.link.fr', 'A venir', '5');
-INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Durée) VALUES ('Soirée N°6', '6 Rue Mozart','2020-06-06 23:00:00', 'Alice In Wonderland', '65', '../Images/Affiches/affiche_soiree6.jpg', '600', 'Billeterie.link.fr', 'Salle', 'DJ 6', 'dj.link.fr', 'A venir', '6');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°1', '1 Rue Mozart','2020-06-01', 'Alice In Wonderland', '15', '../Images/Affiches/affiche_soiree1.jpg', '100', 'Billeterie.link.fr', 'Bar', 'DJ 1', 'dj.link.fr', 'A venir', 'Lizard Lounge', '23h', '05h', '100');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°2', '2 Rue Mozart','2020-06-02', 'Alice In Wonderland', '25', '../Images/Affiches/affiche_soiree2.jpg', '200', 'Billeterie.link.fr', 'Bar', 'DJ 2', 'dj.link.fr', 'A venir', 'Shoteria', '23h', '05h', '200');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°3', '3 Rue Mozart','2020-06-03', 'Alice In Wonderland', '35', '../Images/Affiches/affiche_soiree3.jpg', '300', 'Billeterie.link.fr', 'Salle', 'DJ 3', 'dj.link.fr', 'A venir', 'Salle Wagram', '23h', '05h', '300');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°4', '4 Rue Mozart','2020-06-04', 'Alice In Wonderland', '45', '../Images/Affiches/affiche_soiree4.jpg', '400', 'Billeterie.link.fr', 'Salle', 'DJ 4', 'dj.link.fr', 'A venir', 'Le Life Paris', '23h', '05h', '400');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°5', '5 Rue Mozart','2020-06-05', 'Alice In Wonderland', '55', '../Images/Affiches/affiche_soiree5.jpg', '500', 'Billeterie.link.fr', 'Salle', 'DJ 5', 'dj.link.fr', 'A venir', 'My Boat', '23h', '05h', '500');
+INSERT INTO soirees (Nom, Adresse, Date, Theme, Prix, Affiche, Places, Billeterie, Lieu_type, DJ, DJ_lien, Etat, Lieu_nom, Heure_début, Heure_fin, Places_restantes) VALUES ('Soirée N°6', '6 Rue Mozart','2020-06-06', 'Alice In Wonderland', '65', '../Images/Affiches/affiche_soiree6.jpg', '600', 'Billeterie.link.fr', 'Salle', 'DJ 6', 'dj.link.fr', 'A venir', 'Le Carré Montparnasse', '23h', '05h', '600');
 
 DELETE FROM Organisateurs;
 ALTER TABLE Organisateurs AUTO_INCREMENT =0;
