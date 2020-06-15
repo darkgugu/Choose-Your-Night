5fb70b2ae7c4ad2c2de0f44a14286d83322dd26e
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2020-06-15 16:16:07 +0200

    Ajout d'une base de code

---
+++ Code/base.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+?>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+        <body>
+            <?php
+            
+            ?>
+
+            <?php include 'sidebar.php';?>
+        </body>
+</html>
